c8a9abe735b06c19f7b922b49cf31dd7a48086cb
Knuxfan24 <knuxfan@live.co.uk>
2019-06-21 15:37:42 +0100

    Added a quick tooltip to the Mod Creator.
    Also copied over the .gitignore from the Toolkit because I'm too lazy to fetch the up to date base .gitignore for Visual Studio.

--- Sonic '06 Mod Manager/ModCreate.Designer.cs
+++ Sonic '06 Mod Manager/ModCreate.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ModCreate));
             this.label1 = new System.Windows.Forms.Label();
             this.modTitleBox = new System.Windows.Forms.TextBox();
@@ -39,6 +40,7 @@
             this.modAuthorBox = new System.Windows.Forms.TextBox();
             this.createButton = new System.Windows.Forms.Button();
             this.check_Merge = new System.Windows.Forms.CheckBox();
+            this.mergeTooltip = new System.Windows.Forms.ToolTip(this.components);
             this.SuspendLayout();
             // 
             // label1
@@ -131,6 +133,8 @@
             this.check_Merge.Size = new System.Drawing.Size(62, 17);
             this.check_Merge.TabIndex = 13;
             this.check_Merge.Text = "Merge?";
+            this.mergeTooltip.SetToolTip(this.check_Merge, "Does this mod need to merge its arc files with the base game ones rather than ent" +
+        "irely replace them?");
             this.check_Merge.UseVisualStyleBackColor = true;
             this.check_Merge.CheckedChanged += new System.EventHandler(this.Check_Merge_CheckedChanged);
             // 
@@ -173,5 +177,6 @@
         private System.Windows.Forms.TextBox modAuthorBox;
         private System.Windows.Forms.Button createButton;
         private System.Windows.Forms.CheckBox check_Merge;
+        private System.Windows.Forms.ToolTip mergeTooltip;
     }
 }
