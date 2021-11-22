62d5bd3135d5cb96e74e8d66dc642bb7b5d0ccda
Hyper <HyperPolygon64@gmail.com>
2021-11-22 00:25:01 +0000

    [Converters] InstallState2BooleanConverter
    This is used to disable drag and drop for content in the list currently installing or installed.

--- SonicNextModManager/Metadata/Metadata.cs
+++ SonicNextModManager/Metadata/Metadata.cs
@@ -2,13 +2,6 @@
 
 namespace SonicNextModManager
 {
-    public enum InstallState
-    {
-        Idle,
-        Installing,
-        Installed
-    }
-
     public class Metadata : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler? PropertyChanged;
@@ -29,12 +22,29 @@ namespace SonicNextModManager
         public string? Path { get; set; }
     }
 
+    public enum InstallState
+    {
+        Idle,
+        Installing,
+        Installed
+    }
 
     [ValueConversion(typeof(InstallState), typeof(Visibility))]
     public class InstallState2VisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
             => value.Equals(parameter) ? Visibility.Visible : Visibility.Collapsed;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+
+
+    [ValueConversion(typeof(InstallState), typeof(bool))]
+    public class InstallState2BooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => value.Equals(InstallState.Idle) ? false : true;
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
             => throw new NotImplementedException();
