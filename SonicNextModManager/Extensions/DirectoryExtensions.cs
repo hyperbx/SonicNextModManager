0b89b95f1b8b3a51241818051e6c1d1faf4aab7e
Hyper <HyperPolygon64@gmail.com>
2021-11-22 00:30:29 +0000

    [Extensions] DirectoryExtensions.Contains
    Bear with me again, this was meant to go out with commit 0be6f36.

---
+++ SonicNextModManager/Extensions/DirectoryExtensions.cs
@@ -0,0 +1,37 @@
+﻿namespace SonicNextModManager
+{
+    public class DirectoryExtensions
+    {
+        /// <summary>
+        /// Returns whether or not a directory contains a resulting file based off a wildcard.
+        /// </summary>
+        /// <param name="path">Directory to search.</param>
+        /// <param name="wildcard">Wildcard to use.</param>
+        /// <param name="searchOption">Search option for querying.</param>
+        public static bool Contains(string path, string wildcard, SearchOption searchOption = SearchOption.TopDirectoryOnly)
+        {
+            foreach (string file in Directory.EnumerateFiles(path, wildcard, searchOption))
+                return true;
+
+            return false;
+        }
+
+        /// <summary>
+        /// Returns whether or not a directory contains a resulting file based off a wildcard.
+        /// </summary>
+        /// <param name="path">Directory to search.</param>
+        /// <param name="wildcard">Wildcard to use.</param>
+        /// <param name="single">First result to output.</param>
+        public static bool Contains(string path, string wildcard, out string single)
+        {
+            foreach (string file in Directory.EnumerateFiles(path, wildcard, SearchOption.TopDirectoryOnly))
+            {
+                single = file;
+                return true;
+            }
+
+            single = string.Empty;
+            return false;
+        }
+    }
+}
